53208833dd7fd20a2e9c106c8b8cd254d565cf4a
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 13:50:04 -0600

    Make spec test for character set class

---
+++ spec/character_set_spec.rb
@@ -0,0 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+require 'rspec'
+require 'character_set'
+
+RSpec.describe CharacterSet do
+  it 'exists' do
+    character_set = CharacterSet.new
+    expect(character_set.set).to be_a(CharacterSet)
+  end
+end
