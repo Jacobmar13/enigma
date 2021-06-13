85882b2b9b2866e9edf701323ab069044bf2c1c9
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 14:51:28 -0600

    make tests for method generate_key

--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe KeyGenerator do
     key_generator = KeyGenerator.new
     expect(key_generator).to be_a(KeyGenerator)
   end
+
+  it 'can generate a random five digit key' do
+    key_generator = KeyGenerator.new
+    key_generator.generate_key
+    expect(key_generator.key.length).to eq(5)
+  end
 end
