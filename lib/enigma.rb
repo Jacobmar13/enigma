185ea1065f4d2952bca7b4f6024eaf33dbacb546
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 14:41:31 -0600

    Add structure for encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,12 @@ class Enigma
 
   def initialize
   end
+
+  def encrypt(message, key, date = Time.now.strftime("%d%m%y"))
+    encryption = Hash.new
+    encryption[:encryption] = message
+    encryption[:key] = key
+    encryption[:date] = date
+    encryption
+  end
 end
