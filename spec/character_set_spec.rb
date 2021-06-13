fb93ec347bc785d396e6f1eeba521f7b58691f62
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 14:04:11 -0600

    Test that character set has a set

--- spec/character_set_spec.rb
+++ spec/character_set_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe CharacterSet do
     character_set = CharacterSet.new
     expect(character_set).to be_a(CharacterSet)
   end
+
+  it 'has a character set' do
+    character_set = CharacterSet.new
+    expect(character_set.set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
 end
