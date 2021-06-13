f6e0fe4a16196b45c201719cac1581b100242214
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 13:58:18 -0600

    Make character_set class, add initialize

---
+++ lib/character_set.rb
@@ -0,0 +1,7 @@
+class CharacterSet
+  attr_reader :set
+
+  def initialize
+    @set = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+  end
+end
