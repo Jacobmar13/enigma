1f7353a6cb7e6dcf920b4f8f92a5558f50b1d4c2
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 14:41:01 -0600

    Add tests for start of encrypt method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,7 +9,23 @@ RSpec.describe Enigma do
     @enigma_1 = Enigma.new
     @enigma_2 = Enigma.new
   end
+
   it 'exists' do
     expect(@enigma_1).to be_a(Enigma)
+  end
+
+  xit 'has an encrypt method' do
+    expect(@enigma_1.encrypt("hello world", "02715", "040895")).to eq({
+           encryption: "keder ohulw",
+           key: "02715",
+           date: "040895"
+         })
+  end
+  it 'can provide a date with no date argument' do
+    expect(@enigma_1.encrypt("hello world", "02715")).to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "130621"
+    })
   end
 end
