e3c439a0c253aea68dfabd0fc16aae383c0a360e
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 16:48:22 -0600

    Make key generator method, refactor for simplicity

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,4 +1,16 @@
 class KeyGenerator
+  attr_reader :seed, :key
+
   def initialize
+    @seed = generate_seed
+    @key = generate_key
+  end
+
+  def generate_key
+    @seed.to_s.rjust(5, "0")
+  end
+
+  def generate_seed
+    rand(0..99999)
   end
 end
