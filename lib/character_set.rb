b6dfb82601495e1082bbafa9e0c79d3b7c284a62
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-13 19:02:54 -0600

    Change to single quote and simplify array

--- lib/character_set.rb
+++ lib/character_set.rb
@@ -2,6 +2,6 @@ class CharacterSet
   attr_reader :set
 
   def initialize
-    @set = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    @set = ('a'..'z').to_a << ' '
   end
 end
