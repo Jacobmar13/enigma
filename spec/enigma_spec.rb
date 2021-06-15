834d9bafee7088457cf67fec58e944d0c83c876a
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-14 20:34:02 -0600

    Fix incorrectly formatted test expects

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -4,11 +4,11 @@ require 'rspec'
 require 'enigma'
 require 'character_set'
 require 'encryption'
+require 'decryption'
 
 RSpec.describe Enigma do
   before :each do
     @enigma_1 = Enigma.new
-    @enigma_2 = Enigma.new
   end
 
   it 'exists' do
@@ -22,6 +22,15 @@ RSpec.describe Enigma do
            date: "040895"
          })
   end
+
+  it 'has a decrypt method' do
+    expect(@enigma_1.decrypt("keder ohulw", "02715", "040895")).to eq({
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    })
+  end
+
   it 'can provide a date with no date argument' do
     encrypted = @enigma_1.encrypt("hello world", "02715")
     expect(encrypted[:date]).to eq(Time.now.strftime("%d%m%y"))
